88430e699c2b082a8d768ccf2f96b97b38a6ac20
Morihiko Kanemoto <hi04ko22@gmail.com>
2023-09-11 20:40:04 +0900

    2nd commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const branchName = process.env.BRACH_NAME ? "/" + process.env.BRACH_NAME : "";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: branchName,
+  bacshPath: branchName,
+};
 
 module.exports = nextConfig
