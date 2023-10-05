1e7dd493fb2dfa38878743a579ea5d6541f34053
Morihiko Kanemoto <hi04ko22@gmail.com>
2023-10-06 08:08:08 +0900

    コミット

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const branchName = process.env.BRACH_NAME ? "/" + process.env.BRACH_NAME : "";
-const notionSecrets = process.env.NOTION_SECRETS ? process.env.NOTION_SECRETS : "";
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: branchName,
-  basePath: branchName,
-  notionSecrets: notionSecrets
+  basePath: branchName
 };
 
 module.exports = nextConfig;
