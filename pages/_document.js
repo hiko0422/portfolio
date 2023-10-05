3e573b8bd086d36fbd44417f59e22397f39d3858
Morihiko Kanemoto <hi04ko22@gmail.com>
2023-10-06 08:32:52 +0900

    ファビコンパス修正

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import nextConfig from '@/next.config'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={nextConfig.basePath + "/favicon.ico"} />
       </Head>
       <body>
         <Main />
